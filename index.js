9a94ad6dc033b704acb1f0d547b490cc595a4ed6
Svhodro <rudrolipi@gmail.com>
2024-02-21 01:03:39 +0600

    project complate

---
+++ index.js
@@ -0,0 +1,51 @@
+let searchBtn = document.querySelector(".search");
+let card = document.querySelector(".card")
+let searchInput = document.querySelector(".input")
+let movieImg = document.querySelector(".movie-img")
+let movieName = document.querySelector(".movie-name")
+let movieRateing = document.querySelector(".rateing")
+let releseDate = document.querySelector(".r-date")
+let duretion = document.querySelector(".dureton")
+let cost = document.querySelector(".cost")
+let plot = document.querySelector(".plot")
+
+//fungtion To fatch movie detail's
+
+
+// adding event letionar to search bar
+searchBtn.addEventListener("click", function () {
+    if (searchInput.value !== "") {
+        card.classList.remove("hidden")        
+        fetchData(searchInput.value);
+    }
+
+
+
+
+})
+
+async function fetchData(movie) {
+    try {
+
+        let apiKey = "47b3c25c";
+        let apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=${movie}`;
+        const response = await fetch(apiUrl); // Replace with your API endpoint
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json(); // Parse the response body as 
+        console.log('Fetched data:', data);
+        movieImg.src = data.Poster;
+        movieName.innerHTML = data.Title;
+        movieRateing.innerHTML = data.Rated;
+        releseDate.innerHTML = data.Released;
+        duretion.innerHTML = data.Runtime;
+        cost.innerHTML = data.Genre
+        plot.innerHTML = data.Plot
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
